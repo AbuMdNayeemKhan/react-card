16eda0f91a3799eec846a6f82b801aab2d533a1a
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2022-11-06 20:33:21 +0600

    Learning REACT

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const todoTitle = "Start React";
+const todoDis ="Today is tarted to learning react js. React js is a free and open-source front-end JavaScript library. Today is tarted to learning react js. React js is a free and open-source front-end JavaScript library. Today is tarted to learning react js. React js is a free and open-source front-end JavaScript library.";
+const todoDate = "Nov/3/2022";
+const dateText = "Current year is:";
+const thisYearName = new Date().getFullYear();
+const thisMonthName = new Date().getMonth();
+const thisDayName = new Date().getDay();
+
+// const headingStyle = {
+//   fontSize: "36px",
+//   color: "red",
+//   backgroundColor: "green",
+//   textAlign: "center",
+//   padding: "30px 0"
+// }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div class="card">
+    <h1 className='card-heading' >{todoTitle}</h1>
+    <p className='card-dis'>{todoDis}</p>
+    <p className='card-date'>{thisDayName}/{thisMonthName}/{thisYearName}</p>
+  </div>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
